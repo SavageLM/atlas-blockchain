2af666cfd4aa52bb04e58751e4385bd5bfdfebce
SavageLM <nagol.egavas@gmail.com>
2024-09-10 17:58:52 -0500

    Added code to ec_sign.c

---
+++ crypto/ec_sign.c
@@ -0,0 +1,23 @@
+#include "hblk_crypto.h"
+
+/**
+ * ec_sign - Signs an EC_KEY
+ * @key: key to sign
+ * @msg: characters to be signed
+ * @msglen: number of characters to sign
+ * @sig: address to hold signiture
+ * Return: pointer to sig or NULL
+ */
+uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen,
+				 sig_t *sig)
+{
+	if (!key || !msg)
+		return (NULL);
+	if (!ECDSA_sign(0, msg, msglen, sig->sig, (unsigned int *)&sig->len,
+					(EC_KEY *)key))
+	{
+		sig->len = 0;
+		return (NULL);
+	}
+	return (sig->sig);
+}
