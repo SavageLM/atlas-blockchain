f5f63e948f6e8c8a2d5933f9ddff4411e8a3801e
SavageLM <nagol.egavas@gmail.com>
2024-09-10 15:11:09 -0500

    Forgot a NULL check for pub

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -11,6 +11,8 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 	EC_POINT *point = NULL;
 	const EC_GROUP *group = NULL;
 
+	if (!pub)
+		return (NULL);
 	key = EC_KEY_new_by_curve_name(EC_CURVE);
 	if (!key)
 		return (NULL);
