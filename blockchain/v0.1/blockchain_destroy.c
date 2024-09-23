2c181072abee39e31e3ff15df15237c52dad7073
SavageLM <nagol.egavas@gmail.com>
2024-09-23 13:14:56 -0500

    Added code to blockchain_destroy

---
+++ blockchain/v0.1/blockchain_destroy.c
@@ -0,0 +1,13 @@
+#include "blockchain.h"
+
+/**
+ * blockchain_destroy - destroys a blockchain
+ * @blockchain: chain to destroy
+ */
+void blockchain_destroy(blockchain_t *blockchain)
+{
+	if (!blockchain)
+		return;
+	llist_destroy(blockchain->chain, 1, NULL );
+	free(blockchain);
+}
