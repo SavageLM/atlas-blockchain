d3780ddfd0b3c913fef8a0e8dbd4f6c8bb267408
SavageLM <nagol.egavas@gmail.com>
2024-09-23 13:15:40 -0500

    COrrected betty style

--- blockchain/v0.1/blockchain_destroy.c
+++ blockchain/v0.1/blockchain_destroy.c
@@ -8,6 +8,6 @@ void blockchain_destroy(blockchain_t *blockchain)
 {
 	if (!blockchain)
 		return;
-	llist_destroy(blockchain->chain, 1, NULL );
+	llist_destroy(blockchain->chain, 1, NULL);
 	free(blockchain);
 }
