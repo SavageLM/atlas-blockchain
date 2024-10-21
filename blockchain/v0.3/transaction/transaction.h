415bd7052c3a4ebda930e46d2df6ab63b8c4fb88
SavageLM <nagol.egavas@gmail.com>
2024-10-21 13:06:22 -0500

    Setup v0.3 and transaction repo, added code to tx_out_create

---
+++ blockchain/v0.3/transaction/transaction.h
@@ -0,0 +1,189 @@
+#ifndef _BLOCKCHAIN_H_
+#define _BLOCKCHAIN_H_
+
+#include "provided/endianness.h"
+#include "blockchain.h"
+#include <hblk_crypto.h>
+#include <openssl/sha.h>
+#include <llist.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <time.h>
+
+/* Macros */
+#define COINBASE_AMOUNT 50
+#define BLOCKCHAIN_DATA_MAX 1024
+
+
+/* Structs */
+
+/**
+ * struct blockchain_s - Blockchain structure
+ *
+ * @chain:   Linked list of Blocks
+ * @unspent: Linked list of unspent transaction outputs
+ */
+typedef struct blockchain_s
+{
+	llist_t     *chain;
+	llist_t     *unspent;
+} blockchain_t;
+
+/**
+ * struct block_data_s - Block data
+ *
+ * @buffer: Data buffer
+ * @len:    Data size (in bytes)
+ */
+typedef struct block_data_s
+{
+    /*
+     * @buffer must stay first, so we can directly use the structure as
+     * an array of char
+     */
+	int8_t      buffer[BLOCKCHAIN_DATA_MAX];
+	uint32_t    len;
+} block_data_t;
+
+/**
+ * struct block_info_s - Block info structure
+ *
+ * @index:      Index of the Block in the Blockchain
+ * @difficulty: Difficulty of proof of work (hash leading zero bits)
+ * @timestamp:  Time the Block was created at (UNIX timestamp)
+ * @nonce:      Salt value used to alter the Block hash
+ * @prev_hash:  Hash of the previous Block in the Blockchain
+ */
+typedef struct block_info_s
+{
+	/*
+		* The order of the elements in this structure should remain the same.
+		* It was designed so every element of this structure is aligned and
+		* doesn't require padding from the compiler.
+		* Therefore, it is possible to use the structure as an array of char,
+		* on any architecture.
+		*/
+	uint32_t    index;
+	uint32_t    difficulty;
+	uint64_t    timestamp;
+	uint64_t    nonce;
+	uint8_t     prev_hash[SHA256_DIGEST_LENGTH];
+} block_info_t;
+
+/**
+ * struct block_s - Block structure
+ *
+ * @info:         Block info
+ * @data:         Block data
+ * @transactions: List of transactions
+ * @hash:         256-bit digest of the Block, to ensure authenticity
+ */
+typedef struct block_s
+{
+	block_info_t    info; /* This must stay first */
+	block_data_t    data; /* This must stay second */
+	llist_t     *transactions;
+	uint8_t     hash[SHA256_DIGEST_LENGTH];
+} block_t;
+
+/**
+ * struct transaction_s - Transaction structure
+ *
+ * @id:      Transaction ID. A hash of all the inputs and outputs.
+ *           Prevents further alteration of the transaction.
+ * @inputs:  List of `tx_in_t *`. Transaction inputs
+ * @outputs: List of `tx_out_t *`. Transaction outputs
+ */
+typedef struct transaction_s
+{
+	uint8_t     id[SHA256_DIGEST_LENGTH];
+	llist_t     *inputs;
+	llist_t     *outputs;
+} transaction_t;
+
+/**
+ * struct tx_out_s - Transaction output
+ *
+ * @amount: Amount received
+ * @pub:    Receiver's public address
+ * @hash:   Hash of @amount and @pub. Serves as output ID
+ */
+typedef struct tx_out_s
+{
+	uint32_t    amount;
+	uint8_t     pub[EC_PUB_LEN];
+	uint8_t     hash[SHA256_DIGEST_LENGTH];
+} tx_out_t, to_t;
+
+/**
+ * struct tx_in_s - Transaction input
+ *
+ * Description: A transaction input always refers to a previous
+ * transaction output. The only exception is for a Coinbase transaction, that
+ * adds new coins to ciruclation.
+ *
+ * @block_hash:  Hash of the Block containing the transaction @tx_id
+ * @tx_id:       ID of the transaction containing @tx_out_hash
+ * @tx_out_hash: Hash of the referenced transaction output
+ * @sig:         Signature. Prevents anyone from altering the content of the
+ *               transaction. The transaction input is signed by the receiver
+ *               of the referenced transaction output, using their private key
+ */
+typedef struct tx_in_s
+{
+	uint8_t     block_hash[SHA256_DIGEST_LENGTH];
+	uint8_t     tx_id[SHA256_DIGEST_LENGTH];
+	uint8_t     tx_out_hash[SHA256_DIGEST_LENGTH];
+	sig_t       sig;
+} tx_in_t, ti_t;
+
+/**
+ * struct unspent_tx_out_s - Unspent transaction output
+ *
+ * Description: This structure helps identify transaction outputs that were not
+ * used in any transaction input yet, making them "available".
+ *
+ * @block_hash: Hash of the Block containing the transaction @tx_id
+ * @tx_id:      ID of the transaction containing @out
+ * @out:        Copy of the referenced transaction output
+ */
+typedef struct unspent_tx_out_s
+{
+	uint8_t     block_hash[SHA256_DIGEST_LENGTH];
+	uint8_t     tx_id[SHA256_DIGEST_LENGTH];
+	tx_out_t    out;
+} unspent_tx_out_t, uto_t;
+
+/* Prototypes */
+
+to_t *tx_out_create(
+	uint32_t amount, uint8_t const pub[EC_PUB_LEN]);
+uto_t *unspent_tx_out_create(
+	uint8_t block_hash[SHA256_DIGEST_LENGTH], uint8_t tx_id[SHA256_DIGEST_LENGTH],
+	to_t const *out);
+ti_t *tx_in_create(uto_t const *unspent);
+uint8_t *transaction_hash(
+	transaction_t const *transaction, uint8_t hash_buf[SHA256_DIGEST_LENGTH]);
+sig_t *tx_in_sign(
+	ti_t *in, uint8_t const tx_id[SHA256_DIGEST_LENGTH], EC_KEY const *sender,
+	llist_t *all_unspent);
+transaction_t *transaction_create(
+	EC_KEY const *sender, EC_KEY const *receiver, uint32_t amount,
+	llist_t *all_unspent);
+int transaction_is_valid(
+	transaction_t const *transaction, llist_t *all_unspent);
+transaction_t *coinbase_create(
+	EC_KEY const *receiver, uint32_t block_index);
+int coinbase_is_valid(
+	transaction_t const *coinbase, uint32_t block_index);
+void transaction_destroy(transaction_t *transaction);
+llist_t *update_unspent(
+	llist_t *transactions, uint8_t block_hash[SHA256_DIGEST_LENGTH],
+	llist_t *all_unspent);
+
+
+#endif
