18f5b1cedc45fb1df4f68ecc69d0745999dba0b5
SavageLM <nagol.egavas@gmail.com>
2024-10-23 12:54:27 -0500

    Corrected buffer overwrite by passing in buffer + block size

--- blockchain/v0.3/block_hash.c
+++ blockchain/v0.3/block_hash.c
@@ -13,7 +13,7 @@ int tx_id_cpy(llist_node_t tx, unsigned int iter, void *buffer);
 uint8_t *block_hash(block_t const *block,
 					uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
-	size_t num_tx = 0, buff_len = 0;
+	size_t num_tx = 0, buff_len = 0, block_sz = 0;
 	uint8_t *buffer;
 
 	if (!block || !hash_buf)
@@ -23,10 +23,11 @@ uint8_t *block_hash(block_t const *block,
 		num_tx = llist_size(block->transactions);
 
 	buff_len = sizeof(block->info) + BDL + (num_tx * SHA256_DIGEST_LENGTH);
+	block_sz = sizeof(block->info) + BDL;
 	buffer = calloc(1, buff_len);
 
-	memcpy(buffer, block, sizeof(block->info) + BDL);
-	llist_for_each(block->transactions, tx_id_cpy, buffer);
+	memcpy(buffer, block, block_sz);
+	llist_for_each(block->transactions, tx_id_cpy, buffer + block_sz);
 	SHA256(buffer, buff_len, hash_buf);
 
 	return (hash_buf);
