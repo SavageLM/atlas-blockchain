ce242527039e2390d96312e8bc4aa41b1d04e28d
SavageLM <nagol.egavas@gmail.com>
2024-10-23 12:43:43 -0500

    Updated block hash to hash transactions as well

--- blockchain/v0.3/block_hash.c
+++ blockchain/v0.3/block_hash.c
@@ -1,5 +1,7 @@
 #include "blockchain.h"
 
+#define BDL block->data.len
+
 /**
  * block_hash - hashes a block using sha256
  * @block: block to hash
@@ -9,9 +11,36 @@
 uint8_t *block_hash(block_t const *block,
 					uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
+	size_t num_tx = 0, buff_len = 0;
+	uint8_t *buffer;
+
 	if (!block || !hash_buf)
 		return (NULL);
-	SHA256((unsigned char *)block, sizeof(block->info) + block->data.len,
-			hash_buf);
+
+	if (block->transactions)
+		num_tx = llist_size(block->transactions);
+
+	buff_len = sizeof(block->info) + BDL + (num_tx *SHA256_DIGEST_LENGTH);
+	buffer = calloc(0, buff_len);
+
+	memcpy(buffer, block, sizeof(block->info) + BDL);
+	llist_for_each(block->transactions, tx_id_cpy, buffer);
+	SHA256(buffer, buff_len, hash_buf);
+
 	return (hash_buf);
+}
+
+/**
+ * tx_id_cpy - function to copy tx_id into a buffer
+ * @tx: Transaction to store to buffer
+ * @iter: index of transaction
+ * @buffer: Buffer to copy into
+ * Return: 1 on Fail, otherwise 0
+ */
+int tx_id_cpy(llist_node_t tx, int iter, void *buffer)
+{
+	memcpy(
+		(uint8_t *)buffer + iter * SHA224_DIGEST_LENGTH,
+		((transaction_t *)tx)->id, SHA224_DIGEST_LENGTH);
+	return (0);
 }
